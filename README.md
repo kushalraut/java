099f303b4c3a34ffd50e227e7ff040d926565ef7
kushalraut <kushaldr2001@gmail.com>
2020-09-12 14:26:22 +0530

    1

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # java
  Java Programs
 P-1 : Addition of Two Numbers
-P-2 : Command Line Arguments 
+P-2a : Command Line Arguments 
+P-2b : Addition using Command Line Arguments
---
+++ add_command_line_args.java
@@ -0,0 +1,12 @@
+public class add_command_line_args {
+    public static void main(String args[]) {
+        int c=40;
+        for(int i=0;i<args.length;i++)
+
+        {
+                    c=c + Integer.parseInt(args[i]);
+            
+        }
+         System.out.println("The result is:"+c);
+    }
+}
