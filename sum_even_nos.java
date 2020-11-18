c9546b3b6d800cc1c1ced93eba8d4084d46e94b1
kushalraut <kushaldr2001@gmail.com>
2020-11-18 23:29:17 +0530

    Update sum_even_nos.java
    using do while loop

--- sum_even_nos.java
+++ sum_even_nos.java
@@ -1,18 +1,14 @@
 //Program No.11: Display sum of first 10 even nos.using do-while loop. 
 public class sum_even_nos {
-public static void main(String args[])
-{
-int no=1,count=0,sum=0;
-System.out.println("even number:");
-while(count!=10)
-{
-if(no%2==0)
-{
-sum=sum+no;
-count=count+1;
-}
-no++;
-}
-System.out.println("The sum of  first 10 even numbers is: "+sum);
-}
+    public static void main(String[] args) {
+        int x=0,sum=0,count=0;
+       do{
+           sum=sum+x;
+           x=x+2;
+           count=count+1;
+       }while(count<=10);
+       System.out.println("Sum of first 10 even numbers is "+sum);
+
+    }
+    
 }
